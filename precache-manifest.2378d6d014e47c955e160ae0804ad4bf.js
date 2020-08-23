37eee1474b4363fb33e704445523b1f7a3c3c676
Anuj Jain <jainanuj0812@gmail.com>
2020-08-23 14:19:34 +0530

    Updates

---
+++ precache-manifest.2378d6d014e47c955e160ae0804ad4bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a1780a36aa7cbb700861f7376e6cde6",
+    "url": "/piccuu-clothing/index.html"
+  },
+  {
+    "revision": "89937c690c658052f453",
+    "url": "/piccuu-clothing/static/css/main.2e560f18.chunk.css"
+  },
+  {
+    "revision": "fd4462e0d55758d840e6",
+    "url": "/piccuu-clothing/static/js/2.a5d06386.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/piccuu-clothing/static/js/2.a5d06386.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89937c690c658052f453",
+    "url": "/piccuu-clothing/static/js/main.e9797a75.chunk.js"
+  },
+  {
+    "revision": "18383906ee9635532a50",
+    "url": "/piccuu-clothing/static/js/runtime-main.976fbc75.js"
+  }
+]);
