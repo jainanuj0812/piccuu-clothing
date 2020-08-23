f2504177fdf88990ab5c25c9180ddd94aa4be1c9
Anuj Jain <jainanuj0812@gmail.com>
2020-08-23 14:35:19 +0530

    updated deploy script

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/picuu-clothing">
     <App />
   </BrowserRouter>,
   document.getElementById('root')
